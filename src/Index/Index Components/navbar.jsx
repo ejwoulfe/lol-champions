6363b5befda40200cba0dce69d9039942124383a
Ed Woulfe <edwoulfe@gmail.com>
2019-06-27 19:39:54 -0500

    Creation of Navigation bar and Carousel

---
+++ src/Index/Index Components/navbar.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+
+class NavigationBar extends Component {
+  state = {
+    links: [
+      { name: "Champions", extension: "/champions" },
+      { name: "Role Specific", extension: "/role_specific" }
+    ],
+    logo: "LoL Champions"
+  };
+  renderLinkLists() {
+    const links = this.state.links;
+    const mapOfLinks = links.map(link => (
+      <li className="nav-item">
+        <a className="nav-link" href={link.extension}>
+          {link.name}
+        </a>
+      </li>
+    ));
+    return mapOfLinks;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+          <a className="navbar-brand" href="./index.html">
+            {this.state.logo}
+          </a>
+          <form className="form-inline">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search for Champion"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">{this.renderLinkLists()}</ul>
+            <button id="login_button">Login</button>
+            <button id="sign_up_button">Sign Up</button>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import NavigationBar from "./Index/Index Components/navbar";
+import Carousel from "./Index/Index Components/carousel";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavigationBar />, document.getElementById("nav"));
+ReactDOM.render(<Jumbotron />, document.getElementById("jumbotron_container"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
