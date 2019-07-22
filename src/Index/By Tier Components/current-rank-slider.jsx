31a9e93fc755e8bdc65b0d1cc6925195a7a09d81
Ed Woulfe <edwoulfe@gmail.com>
2019-07-22 14:14:01 -0500

    Fetch Requests seperated into according methods.

--- src/Index/By Tier Components/current-rank-slider.jsx
+++ src/Index/By Tier Components/current-rank-slider.jsx
@@ -7,12 +7,61 @@ class CurrentRankSlider extends Component {
     summonerIds: [],
     accountIds: []
   };
-  buildAccountIdsArray(summonerIds) {
-    for (var index in summonerIds) {
-      fetch(
-        "https://cors-anywhere.herokuapp.com/" +
+
+  fetchSummonerIds(rank) {
+    if (rank === "Master" || rank === "Grandmaster" || rank === "Challenger") {
+      const riotURL =
+        "https://na1.api.riotgames.com/lol/league/v4/" +
+        rank.toLowerCase() +
+        "leagues/by-queue/RANKED_SOLO_5x5?api_key=";
+      const fetchURL = String(proxyurl + riotURL + key);
+      this.buildSummonerIdsArray("master+", fetchURL);
+    } else {
+      const riotURL =
+        "https://na1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/" +
+        rank.toUpperCase() +
+        "/II?api_key=";
+      const fetchURL = String(proxyurl + riotURL + key);
+      this.buildSummonerIdsArray("ironToDiamond", fetchURL);
+    }
+  }
+  async buildSummonerIdsArray(type, fetchURL) {
+    if (type === "master+") {
+      await fetch(fetchURL)
+        .then(result => {
+          return result.json();
+        })
+        .then(result => {
+          for (var index = 0; index < 20; index++) {
+            this.setState({
+              summonerIds: [
+                ...this.state.summonerIds,
+                result.entries[index].summonerId
+              ]
+            });
+          }
+        });
+    } else {
+      await fetch(fetchURL)
+        .then(result => {
+          return result.json();
+        })
+        .then(result => {
+          for (var index = 0; index < 20; index++) {
+            this.setState({
+              summonerIds: [...this.state.summonerIds, result[index].summonerId]
+            });
+          }
+        });
+    }
+    this.fetchAccountIds(this.state.summonerIds);
+  }
+  async fetchAccountIds(summonerIds) {
+    for (var ids in summonerIds) {
+      await fetch(
+        proxyurl +
           "https://na1.api.riotgames.com/lol/summoner/v4/summoners/" +
-          summonerIds[index] +
+          summonerIds[ids] +
           "?api_key=" +
           key
       )
@@ -20,83 +69,20 @@ class CurrentRankSlider extends Component {
           return result.json();
         })
         .then(result => {
-          console.log(result);
+          this.setState({
+            accountIds: [...this.state.accountIds, result.accountId]
+          });
         });
     }
+    console.log(this.state.accountIds);
+  }
+  fetchChampionsPlayedBySummoners(accountIds) {
+    console.log(accountIds);
   }
   componentDidMount() {
-    const tierUpperCase = this.props.tier.toUpperCase();
-    let riotURL = "";
-    let idType = "";
-
-    if (this.props.tier === "Master") {
-      riotURL =
-        "https://na1.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key=";
-    } else if (this.props.tier === "Grandmaster") {
-      riotURL =
-        "https://na1.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5?api_key=";
-    } else if (this.props.tier === "Challenger") {
-      riotURL =
-        "https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=";
-    } else {
-      riotURL =
-        "https://na1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/" +
-        tierUpperCase +
-        "/II?api_key=";
-    }
-
-    const fetchURL = String(proxyurl + riotURL + key);
-
-    fetch(fetchURL)
-      .then(res1 => {
-        return res1.json();
-      })
-      .then(result1 => {
-        for (var i = 0; i < 20; i++) {
-          if (idType === "account") {
-          } else {
-            this.setState({
-              summonerIds: [
-                ...this.state.summonerIds,
-                result1.entries[i].summonerId
-              ]
-            });
-          }
-        }
-
-        this.buildAccountIdsArray(this.state.summonerIds);
-      });
-    // .then(result => {
-    //   return result.json();
-    // })
-    // .then(result => {
-    //   const test1 =
-    //     proxyurl +
-    //     "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/" +
-    //     result.accountId +
-    //     "?api_key=" +
-    //     key;
-    //   return fetch(test1);
-    // })
-    // .then(result => {
-    //   return result.json();
-    // })
-    // .then(result => {
-    //   console.log(result.matches);
-    // });
-
-    // fetch(
-    //   proxyurl +
-    //     "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" +
-    //     this.state.summonerIds[0] +
-    //     "?api_key=" +
-    //     key
-    // )
-    //   .then(res => res.json())
-    //   .then(result => {
-    //     console.log(result);
-    //   });
+    this.fetchSummonerIds(this.props.tier);
   }
+
   render() {
     return (
       <div>
